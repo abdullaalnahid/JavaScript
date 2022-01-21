b2219621fda99dde5f0f9ba9f918878120fdeb54
abdulla <abdullaalnahidctg@gmail.com>
2022-01-22 01:32:46 +0600

    JS Arithmetic

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+
+//------------
+var x = 5;
+var y = 7;
+var z = x+y;
+
+document.getElementById("nahid").innerHTML="The value of z is: " +z;
+
+//--------------
+const nahid = 22;
+const nayem = 11;
+let total = nahid - nayem;
+
+document.getElementById("nahid1").innerHTML="The value of a is: " +total;
+
+
+//---------------
+let don = "Ding";
+let don1 = "Dong";
+let nahid2 = don + " " + don1;
+
+document.getElementById("jhon").innerHTML= nahid2;
+
+
+// ----------------
+var x = 22
+x -= 5
+
+document.getElementById("try").innerHTML= x
+
+
+//-------------------
+let a = 5 + 5;
+let b = "7" + 2;
+let c = "Nahid" + 32;
+let d = a + a;
+
+document.getElementById("not").innerHTML= a + "<br>" + b + "<br>" + "<br>" + c + "<br>" + d;
+
+
+//----------------------
+let txt = "It's Abdulla Al"
+txt += " Nahid"
+document.getElementById("yes").innerHTML= txt;
+
+
+
+
+
