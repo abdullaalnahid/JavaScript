9707a03b5f44c6456ea90ecad43c002b6de07e04
Abdulla Al Nahid <97011163+abdullaalnahid@users.noreply.github.com>
2022-01-25 00:37:14 +0600

    Loop Statements

---
+++ Nahid's work Cl-8/Loop Statemet/loop.js
@@ -0,0 +1,50 @@
+//-------------------
+document.write("<h1>1st Loop start</h1>")
+
+for (let a=1; a <=5; a++){
+    document.write("<h3>Bahgladesh is a good country.</h3>")
+}
+
+
+//--------------------
+document.write("<h1>2nd Loop start</h1>")
+
+for (let a=1; a <= 10; a=a+1){
+    document.write(" " + a)
+}
+
+
+//--------------------
+document.write("<h1>3rd Loop start</h1>")
+
+for (let a=1; a<= 99; a=a+2){
+    document.write(" " + a)
+}
+
+
+//-----------------------
+document.write("<h1>4th Loop start</h1>")
+
+for (var a= 2; a<=100; a=a+2){
+    document.write(" " +a)
+}
+
+
+//-----------------------------
+document.write("<h1>5th Loop start</h1>")
+
+for(let b= 10; b>= 1; b--)
+document.write(" " +b)
+
+
+//-------------------------
+let num = 6;
+
+if(num % 2 == 0){
+    console.log("Even")
+}
+
+else{
+    console.log("Odd")
+}
+
