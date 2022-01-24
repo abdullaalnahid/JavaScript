b6c152df8fc6a77285145c684657f630395256f2
Abdulla Al Nahid <97011163+abdullaalnahid@users.noreply.github.com>
2022-01-24 19:11:56 +0600

    Add files via upload

---
+++ Nahid's work Cl-8/condition.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 15
+
+if (a>=b){
+    document.write("A is Greater")
+}
+else{
+    document.write("B is Greater")
+}
---
+++ Nahid's work Cl-8/condition1.js
@@ -0,0 +1,13 @@
+let a = 5
+let b = 10 
+let c = 15
+
+if (a>=b){
+    document.write("A is Greater")
+}
+else if (b>=c){
+    document.write("B is Greater")
+}
+else {
+    document.write("C is Greater")
+}
